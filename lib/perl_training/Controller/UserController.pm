d481f9ed63bb90102783b14464a2456028b6cc00
quantinh <htinh7444@gmail.com>
2023-06-19 12:00:35 +0700

    fix password error

--- lib/perl_training/Controller/UserController.pm
+++ lib/perl_training/Controller/UserController.pm
@@ -27,63 +27,61 @@ sub index ($self) {
 # Action form Login
 sub form_login($self) {
   #Invalid error
-  my $error = $self->flash('error');
-  #Invalid error
   $self->render(
     template  => 'layouts/login',
-    error     => $error
   );
 }
 
 # Action Login
-sub login($self) {
-  my $error = {};
-  my $email; 
-  my $password;
-  my $get_user;
-  # Check email defined
-  if (not defined $self->param('email') or $self->param('email') eq '') {
-    $error->{email} = 'Email không được để trống !';
-  } else {
-    my $email_validator = Email::Valid->new();
-    unless ($email_validator->address($self->param('email'))) {
-      $error->{email} = 'Email chưa đúng định dạng!';
-    } else {
-      $email = $self->param('email');
-    }
-  }
-  # Check password defined
-  if (not defined $self->param('password') or $self->param('password') eq '') {
-    $error->{password} = 'Password không được để trống !';
-  } else {
-    if ($self->param('password') !~ /^[A-Za-z0-9_\.]{3,32}$/) {
-      $error->{password} = 'Password chưa đúng định dạng !';
-    } else {
-      $password = $self->param('password');
-    }
-  }
-  # Check not error and insert to database
-  if (not $error) {
-    # Check email có tồn tại trên database hay không tức là đã đăng kí trước đó thì mới cho vào 
-    # $get_user = $self->rs('User')->search({ email => $email })->first;
-    # if ($get_user && $self->validate_password($get_user->get_column('password'), $password)) {
-      $self->session(logged_in => 1);   # Setting sesion logged_in = 1
-      $self->session(experation => 600);  # Settings sesion timeout 600s 
-      $self->flash(success => 'Đăng nhập thành công');
-      return $self->redirect_to('/');
-    } else {
-      $self->flash(error => 'Email hoặc mật khẩu không chính xác!');
-      return $self->redirect_to('/form_login');
-    }
-  # } 
-}
+# sub login($self) {
+#   my $error = {};
+#   my $email; 
+#   my $password;
+#   my $get_user;
+#   # Check email defined
+#   if (not defined $self->param('email') or $self->param('email') eq '') {
+#     $error->{email} = 'Email không được để trống !';
+#   } else {
+#     if ($self->param('email') !~ /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/) {
+#       $error->{email} = 'Email chưa đúng định dạng!';
+#     } else {
+#       $email = $self->param('email');
+#     }
+#   }
+#   # Check password defined
+#   if (not defined $self->param('password') or $self->param('password') eq '') {
+#     $error->{password} = 'Password không được để trống !';
+#   } else {
+#     if ($self->param('password') !~ /^[A-Za-z0-9_\.]{3,32}$/) {
+#       $error->{password} = 'Password chưa đúng định dạng !';
+#     } else {
+#       $password = $self->param('password');
+#     }
+#   }
+#   # Check not error and insert to database
+#   if (not $error) {
+#     # Check email có tồn tại trên database hay không tức là đã đăng kí trước đó thì mới cho vào 
+#     # $get_user = $self->rs('User')->search({ email => $email })->first;
+#     # if ($get_user && $self->validate_password($get_user->get_column('password'), $password)) {
+#       # Setting sesion logged_in = 1
+#       $self->session(logged_in => 1);   
+#       # Settings sesion timeout 600s 
+#       $self->session(experation => 600);
+#       $self->flash(success => 'Đăng nhập thành công');
+#       return $self->redirect_to('/');
+#     } else {
+#       $self->flash(error => 'Email hoặc mật khẩu không chính xác!');
+#       return $self->redirect_to('/form_login');
+#     # }
+#   } 
+# }
 
 # Action Logout 
-sub logout($self) {
-  $self->logout;
-  $self->session(expires => 1);
-  return $self->redirect_to('/form_login');
-}
+# sub logout($self) {
+#   $self->logout;
+#   $self->session(expires => 1);
+#   return $self->redirect_to('/form_login');
+# }
 
 # Action validate form use Email::Valid
 # sub validate_form($self, $email, $password) {
@@ -99,7 +97,7 @@ sub logout($self) {
 #   return @errors;
 # }
 
-# # Action login
+# Action login
 # sub login($self) {
 #   my $email       = $self->param('email');
 #   my $password    = $self->param('password');
@@ -109,7 +107,6 @@ sub logout($self) {
 #     return $self->render(
 #       template  => 'layouts/login',
 #       message   => \@valid_input,
-#       # message   => [@valid_input],
 #     );
 #   }
 #   # Get the email form table users
@@ -128,6 +125,47 @@ sub logout($self) {
 #     );
 #   }
 # }
+
+sub login($self) {
+  my $validation = $self->validation;
+  my $email    = $self->param('email');
+  my $password = $self->param('password');
+  #Check empty email password
+  if(!$email || !$password) {
+    return $self->render(
+      template => 'layouts/login',
+      message => 'Email , password không được để trống !'
+    );
+  #Check valid characters
+  } else {
+    $validation->required('email')->like(qr/^\w+@\w+\.\w+$/);
+    if ($validation->has_error) {
+      return $self->render(
+        template => 'layouts/login',
+        message => 'Email không đúng định dạng !'
+      );
+    }
+    $validation->required('password')->size(6, 20);
+    if ($validation->has_error) {
+      return $self->render(
+        template => 'layouts/login',
+        message => 'Mật khẩu phải từ 6 đến 20 ký tự !'
+      );
+    }
+    my $get_user = $self->rs('User')->search({ email => $email })->first;
+    if ($get_user && $self->validate_password($get_user->get_column('password'), $password)) {
+      return $self->render(
+        template => 'layouts/success',
+        message  => 'Đăng nhập thành công !'
+      );
+    } else {
+      return $self->render(
+        template => 'layouts/login',
+        message  => 'Bạn chưa tạo tài khoản. Vui lòng tạo tài khoản để đăng nhập !'
+      );
+    }
+  }
+}
 
 
 
